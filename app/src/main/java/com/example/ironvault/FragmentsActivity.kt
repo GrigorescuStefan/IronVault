3c77b832eb133b756ce3f9695af2a63b95cea57d
GrigorescuStefan <stefan.grigorescu@student.unitbv.ro>
2024-03-16 01:11:17 +0200

    Fixed Navigation Bar Functionality

--- app/src/main/java/com/example/ironvault/FragmentsActivity.kt
+++ app/src/main/java/com/example/ironvault/FragmentsActivity.kt
@@ -1,21 +1,48 @@
 package com.example.ironvault
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.navigation.findNavController
+import androidx.fragment.app.FragmentActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class FragmentsActivity : ComponentActivity() {
 
+class FragmentsActivity : FragmentActivity() {
+
+    private lateinit var navigationController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.fragments_layout)
-//        val navigationController = findNavController(this, R.id.)
-//        val navigationMenu: BottomNavigationView = findViewById(R.id.bottomNavigationView)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navigationController = navHostFragment.navController
+
+        val navigationMenu: BottomNavigationView = findViewById(R.id.bottomNavigationView)
         val bundledData = intent.getBundleExtra("credentials")
         val credentialsMap = bundledData?.let { UtilityFunctions.convertBundleToMap(it) }
         UtilityFunctions.showToastMessage(this, credentialsMap.toString())
 
-
+        navigationMenu.setOnItemSelectedListener { menuItem ->
+            when(menuItem.itemId){
+                R.id.myVault -> {
+                    navigationController.navigate(R.id.vaultFragment)
+                    true
+                }
+                R.id.generator -> {
+                    navigationController.navigate(R.id.generatorFragment)
+                    true
+                }
+                R.id.scanner -> {
+                    navigationController.navigate(R.id.scannerFragment)
+                    true
+                }
+                R.id.settings -> {
+                    navigationController.navigate(R.id.settingsFragment)
+                    true
+                }
+                else -> false
+            }
+        }
     }
+
 }
